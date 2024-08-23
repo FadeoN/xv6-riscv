d90705d5316f65f117b390ffaa78466f4ac13cc5
onur demir <31078178+FadeoN@users.noreply.github.com>
2024-08-23 17:55:20 +0200

    odemir: refactor primes

--- user/primes.c
+++ user/primes.c
@@ -1,15 +1,18 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+static const int STIN = 0;
+static const int STOUT = 1;
+static const int STERR = 2;
+
+
 void generateNumbers(int startingFrom)
 {
     static const int END_LOOKUP = 35;
-    int buf[1];
 
     for (int i = startingFrom; i < END_LOOKUP; ++i)
     {
-        buf[0] = i;
-        if (write(1, buf, sizeof buf) != sizeof buf)
+        if (write(STOUT, &i, sizeof i) != sizeof i)
         {
             fprintf(2, "could not write");
             exit(1);
@@ -25,7 +28,7 @@ void runWithPrime(void (*aLeftCmd)(int), void (*aRightCmd)(), int prime)
 
     if (fork() == 0)
     {
-        close(0);
+        close(STIN);
         dup(p[0]);
         close(p[1]);
 
@@ -35,7 +38,7 @@ void runWithPrime(void (*aLeftCmd)(int), void (*aRightCmd)(), int prime)
     }
     else
     {
-        close(1);
+        close(STOUT);
         dup(p[1]);
         close(p[0]);
 
@@ -47,27 +50,27 @@ void runWithPrime(void (*aLeftCmd)(int), void (*aRightCmd)(), int prime)
 
 void filterPrimes(int prime)
 {
-    int n[1];
+    int n;
 
-    while (read(0, n, sizeof n) == sizeof n)
+    while (read(STIN, &n, sizeof n) == sizeof n)
     {
 
-        if ((n[0] % prime) != 0)
+        if ((n % prime) != 0)
         {
-            write(1, n, sizeof n);
+            write(STOUT, &n, sizeof n);
         }
     }
 }
 
 void readPrimeAndFilter()
 {
-    int prime[1];
-    if(read(0, prime, sizeof prime) < sizeof prime){
+    int prime;
+    if(read(STIN, &prime, sizeof prime) < sizeof prime){
         exit(0);
     }
-    fprintf(2, "executing %d\n", prime[0]);
+    fprintf(STERR, "executing %d\n", prime);
 
-    runWithPrime(filterPrimes, readPrimeAndFilter, prime[0]);
+    runWithPrime(filterPrimes, readPrimeAndFilter, prime);
 }
 
 int main(int argc, char *argv[])
